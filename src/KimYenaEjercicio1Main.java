a03471ac786eb51769943817e3fc107c17f59f3f
YENA <130546842+Yenakim242@users.noreply.github.com>
2024-12-14 22:32:39 +0100

    Main actualizado

--- src/KimYenaEjercicio1Main.java
+++ src/KimYenaEjercicio1Main.java
@@ -4,22 +4,27 @@ import java.util.Random;
 public class KimYenaEjercicio1Main {
     public static void main(String[] args) {
         // Crear un objeto del tipo KimYenaPerfil
-        KimYenaPerfil miPerfil = new KimYenaPerfil("Yena Kim", "Soy una desarrolladora web apasioinada por el diseño y el arte digital.");
+        KimYenaPerfil miPerfil = new KimYenaPerfil("Yena Kim", "Soy una desarrolladora web apasioinada por el running y el arte.");
+
+        // Añadir información al perfil
         miPerfil.getHobbies().add("\uD83C\uDFC3\u200D♀\uFE0F\u200D➡\uFE0F Running");
-        miPerfil.getHobbies().add("\uD83C\uDFB9 Hacer música");
+        miPerfil.getHobbies().add("\uD83C\uDFB6 Hacer música");
         miPerfil.getHobbies().add("\uD83C\uDFAC Ver película");
         miPerfil.getFoods().add("\uD83C\uDF4C Plátanos");
-        miPerfil.getFoods().add("\uD83C\uDF4C Chicken chicken chicken coreano");
-        miPerfil.getFoods().add("\uD83C\uDF4C Chocolate");
+        miPerfil.getFoods().add("\uD83C\uDF57 Chicken chicken chicken coreano");
+        miPerfil.getFoods().add("\uD83C\uDF6B Chocolate");
         miPerfil.getFunFacts().add("Este año he completado correr 21 km en el half-maratón!");
-        miPerfil.getFunFacts().add("Me gusta producir música y djing");
-        miPerfil.getFunFacts().add("Soy apicionadora de Pokemon");
+        miPerfil.getFunFacts().add("Me gusta producir música y hacer DJ.");
+        miPerfil.getFunFacts().add("Soy una apisionada de Pokemon");
+
         Scanner scanner = new Scanner(System.in);
         int option = 0;
 
+        // Bucle del menú
         while (option != 4) {
             showMenu(miPerfil.getName());
             try {
+                // Leer opción del usuario
                 option = Integer.parseInt(scanner.nextLine());
 
                 switch (option) {
@@ -31,22 +36,26 @@ public class KimYenaEjercicio1Main {
                         System.out.println(miPerfil.getFormattedFoods());
                         break;
                     case 3:
+                        // Obtener un fun fact aleatorio
                         String funFact = miPerfil.getFunFacts().get(new Random().nextInt(miPerfil.getFunFacts().size()));
-                        System.out.println("===Fun Fact ===\n" + funFact);
+                        System.out.println("===Fun Fact ===");
+                        System.out.println(funFact);
                         break;
                     case 4:
-                        System.out.println("¡Adiós!");
+                        System.out.println("\n¡Adiós!");
                         break;
                     default:
-                        System.out.println("Opción no válida. Intenta nuevamente.");
+                        System.out.println("\nOpción no válida. Intenta nuevamente.");
                 }
             } catch (NumberFormatException e) {
-                System.out.println("Entrada no válida. Por favor, introduce un número del 1 al 4.");
+                System.out.println("\nEntrada no válida. Por favor, introduce un número del 1 al 4.");
             }
-            System.out.println();
+            // Separador entre iteraciones
+            System.out.println("--------------------------------------\n");
         }
         scanner.close();
     }
+    // Mostrar el menú
     public static void showMenu(String nombre) {
         System.out.println("Menu AboutMe [" + nombre + "]");
         System.out.println("[1] Story");
