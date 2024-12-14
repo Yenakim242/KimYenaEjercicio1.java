86dd36c2b34e05b41f4998b80bfbf12da979aa64
YENA <130546842+Yenakim242@users.noreply.github.com>
2024-12-14 15:27:09 +0100

    Initial commit

---
+++ src/KimYenaEjercicio1Main.java
@@ -0,0 +1,5 @@
+public class KimYenaEjercicio1Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/KimYenaPerfil.java
@@ -0,0 +1,2 @@
+public class KimYenaPerfil {
+}
